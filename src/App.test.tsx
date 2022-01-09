d456a72c5e7818bbe3d9e479af8f98cb2e046bc3
Andrew Osborne <andrew@Andrews-MacBook-Pro.local>
2022-01-09 14:09:59 +0000

    adding dummy test to check code coverage is working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,3 +6,9 @@ test('simple test to test ci/cd test', () => {
   const number = 5
   expect(number).toBe(5);
 });
+
+test('ensure p tag is rendered on screen', () => {
+  render(<App />)
+  const appParagraphElement = screen.getByText(/app/i)
+  expect(appParagraphElement).toBeInTheDocument()
+})
